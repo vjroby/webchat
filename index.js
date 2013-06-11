ec704accb31979338124f49f847e210e09bc5f24
vjroby <vjroby@gmail.com>
2013-06-11 21:26:13 +0300

    resolved issue client side

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(express.static(__dirname + '/public'));
 
 var io = require('socket.io').listen(app.listen(port));
 
+
+
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
--- public/chat.js
+++ public/chat.js
@@ -2,7 +2,7 @@ window.onload = function() {
 
     var messages = [];
     var usernames = [];
-    var socket = io.connect('http://localhost:3700');
+    var socket = io.connect(document.location.href);
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
